5e1b01f167c1138814cbc197fafdab34faf90713
Anemany <marija.nanojana@gmail.com>
2024-06-10 17:21:27 +0300

    jautajumi

--- main.py
+++ main.py
@@ -2,47 +2,68 @@ class main:
     def __init__(self):
         self.questions = [
             {
-                "question": "как можно возвести в степень в языке програмирования Python?",
-                "options": ["1) **", "2) pow(x,y)", "3) sqrt(x,y)", "4) round(x)"],
-                "answer": "1) **" or "2) pow(x,y)"
+                "question": "Kā Python programmēšanas valodā var paaugstināt skaitli pakāpē?",
+                "options": ["**", "pow(x,y)", "sqrt(x,y)", "round(x)"],
+                "answer": ["1", "2"]
             },
             {
-                "question": "Какой результат будет у выражения 3 * 3 ** 3?",
-                "options": ["27", "81", "243", "729"],
-                "answer": "81"
+                "question": "Kuras no šīm funkcijām prasa 'math' moduli?",
+                "options": ["Modulis", "Kvadrātsakne", "Sinuss", "Minimālā vērtība"],
+                "answer": ["2", "3"]
             },
             {
-                "question": "Как создать функцию в Python?",
-                "options": ["function myFunction() {}", "def myFunction():", "create myFunction():", "func myFunction() {}"],
-                "answer": "def myFunction():"
+                "question": "Kura funkcija NE atgriež tangensu?",
+                "options": ["math.tan(x)", "math.cos(x)", "math.floor(x)", "min(x,y)"],
+                "answer": ["2", "3", "4"]
             },
             {
-                "question": "Какой метод используется для добавления элемента в конец списка?",
-                "options": ["add()", "append()", "insert()", "extend()"],
-                "answer": "append()"
+                "question": "Kādu vērtību atgriež funkcija 'math.pi'?",
+                "options": ["3.141592653589793", "random", "pi skaitlis", "4"],
+                "answer": ["1", "3"]
             },
             {
-                "question": "Какой из следующих методов не изменяет исходный список?",
-                "options": ["sort()", "sorted()", "reverse()", "append()"],
-                "answer": "sorted()"
+                "question": "Cik radiāni ir 180 grādi?",
+                "options": ["pi skaitlis", "3.141592653589793", "2", "360 grādi"],
+                "answer": ["1", "2"]
+            },
+            {
+                "question": "Kādu vērtību atgriež funkcija 'math.sin(math.pi/2)'?",
+                "options": ["0", "0.0", "1", "1.0"],
+                "answer": ["3", "4"]
+            },
+            {
+                "question": "Ko atgriež funkcija 'abs(-5)'?",
+                "options": ["-5", "0", "5", "5.0"],
+                "answer": ["3", "4"]
+            },
+            {
+                "question": "Kā iegūt kvadrātsakni no skaitļa?",
+                "options": ["sqrt = x ** (0.5)", "num = sakne x", "sqrt.math(x)", "math.sqrt(x)"],
+                "answer": ["1", "4"]
+            },
+            {
+                "question": "Ko atgriež 'ceil(4.2)'?",
+                "options": ["4", "4.5", "5", "5.0"],
+                "answer": ["3", "4"]
             }
         ]
         self.score = 0
 
     def run_test(self):
-        print("Тест на знание Python\n")
+        print("Tests par skaitliskajiem maingajiem\n")
         for i, q in enumerate(self.questions, start=1):
-            print(f"Вопрос {i}: {q['question']}")
+            print(f"Jautājums {i}: {q['question']}")
             for j, option in enumerate(q["options"], start=1):
                 print(f"{j}. {option}")
-            answer = input("Ваш ответ (введите номер): ")
-            if q["options"][int(answer) - 1] == q["answer"]:
-                print("Правильно!\n")
+            answer = input("Jūsu atbilde (ievadiet numuru): ")
+            if str(answer) in q["answer"]:
+                print("Pareizi!\n")
                 self.score += 1
             else:
-                print(f"Неправильно. Правильный ответ: {q['answer']}\n")
+                correct_answers = " vai ".join(q["answer"])
+                print(f"Nepareizi. Pareizā atbilde: {correct_answers}\n")
 
-        print(f"Ваш результат: {self.score} из {len(self.questions)}")
+        print(f"Jūsu rezultāts: {self.score} no {len(self.questions)}")
 
 if __name__ == "__main__":
     test = main()
