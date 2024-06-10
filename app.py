b818a32f0ac502335f918d12d1db6c2cd04fda65
Anemany <marija.nanojana@gmail.com>
2024-06-10 21:30:21 +0300

    konsole

--- app.py
+++ app.py
@@ -1,19 +1,3 @@
-from flask import Flask, render_template, request, redirect, url_for
-
-app = Flask(__name__, static_folder="./static", template_folder="./templates")
-app.debug = True
-
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-@app.route('/questions')
-def questions():
-    return render_template('questions.html')
-
-if __name__ == "__main__":    
-    app.run(host='0.0.0.0', port=8080)
-    
 class app:
     def __init__(self):
         self.questions = [
